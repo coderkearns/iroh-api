da5f03b3019b69cf1f04d7fa94476491ef0fa00e
Carter Kearns <coder.kearns@gmail.com>
2022-01-10 10:20:44 -0600

    use better default for port and host

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 80;
+const HOST = process.env.HOST || '0.0.0.0';
 
 const server = app.listen(PORT, HOST, () => {
     console.log(`Server listening at http://${HOST}:${PORT}/`);
